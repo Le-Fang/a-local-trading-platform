bc57aa7c5e794ba2ab53f9a15c0826528158543b
le <eddiii.6k@gmail.com>
2024-09-12 20:59:26 +0800

    update register and verify login

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
-from .login.login import login_bp  # Import the Blueprint
+from app.login.login import login_bp  # Import the Blueprint
+from app.extensions import db
 
 
-app = Flask(__name__, template_folder='templates')
-# app.config.from_object(Config)
+app = Flask(__name__)
 app.secret_key = Config.SECRET_KEY  # Set a secret key for session management
 
 # Database
@@ -15,4 +14,4 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Register Blueprints
 app.register_blueprint(login_bp)
 
-db = SQLAlchemy(app)
+db.init_app(app)
--- app/login/login.py
+++ app/login/login.py
@@ -1,28 +1,45 @@
 from flask import Flask, render_template, request, redirect, Blueprint, flash, url_for
+from app.models import User
+from app.extensions import db
+from app import utils
 
 login_bp = Blueprint('login_bp', __name__)
 
-@login_bp.route('/login', methods=['GET', 'POST'])
+@login_bp.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
-        # Add your authentication logic here
-        if username == 'admin' and password == 'password':
-            flash('You were successfully logged in', 'success')
-            return redirect(url_for('homepage_bp.homepage'))
-        else:
-            flash("Invalid credentials. Please try again.", 'failure')
-            return redirect(url_for('login_bp.login'))
+    username = request.form['username']
+    password = request.form['password']
+
+    # Check if the user exists
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        return {"message": "User does not exist"}
+
+    # Check if the password is correct
+    if user.password_hash != utils.hash_password(password):
+        return {"message": "Incorrect password"}
     
-    return "f"
+    
+    return {"message": "Logged in successfully"}
 
 
-@login_bp.route('/register', methods=['GET', 'POST'])
+@login_bp.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        # Add your registration logic here
-        pass
-    return "a"
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+
+    # Check if the username or email already exists
+    if User.query.filter_by(username=username).first() is not None:
+        return {"message": "username already exists"}
+    
+    if User.query.filter_by(email=email).first() is not None:
+        return {"message": "email already exists"}
+
+    # Create a new user
+    new_user = User(username=username, password_hash=utils.hash_password(password), email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return {"message": "User created successfully"}
 
